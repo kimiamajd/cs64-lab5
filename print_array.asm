97c34a29617d6ce048fed8cccc355ce3cf596e71
“Kimia <kimiamajd@umail.ucsb.edu>
2020-02-10 15:38:36 -0800

    fixed print array

--- print_array.asm
+++ print_array.asm
@@ -6,57 +6,53 @@
 #   make all returned values from functions go in $v0
 
 .data
-	myArray: .word 1 2 3 4 5 6 7 8 9 10
-
-	arrayLength: .word 10
-
-	contents: .asciiz "The contents of the array are:\n"
-
-	endline: .asciiz "\n"
+contents: .asciiz "The contents of the array are:\n"
+newline: .asciiz "\n"
+array: .word 1 2 3 4 5 6 7 8 9 10
+arrayLength: .word 10
 
 .text
-printA:
-    li $t0, 0 # t0 = i = 0
-	move $t1, $a0  # address of array now saved in $t1
-	
-
-
-	loop:
-	slt $t2, $t0, $a1
-	beq $t2, $zero, end_loop
-
-	
-	lw $t3, 0($t1)
-	move $a0, $t3
-	li $v0, 1
-	syscall
-
-	la $a0, endline
-	li $v0, 4
-	syscall
-
-	addi $t1, $t1, 4
-	addi $t0, $t0, 1
-	j loop
-
-
-	end_loop:
-
-	jr $ra
 
 main:
 
-	li $v0, 4
-	la $a0, contents
-	syscall 
+# output "The contents of the array are: "
+la $a0, contents
+li $v0, 4
+syscall
 
-	la $a0, myArray
-	la $t0, arrayLength
-	lw $a1, 0($t0)
+# put array address in $a01, arrayLength in $a02 
+la $a0, array
+lw $a1, arrayLength
 
-	jal printA
+jal printA
 
 
-exit:
-	li $v0, 10
-	syscall
+li $v0, 10
+syscall
+
+printA:
+
+li $t0, 0  			# x is in $t0
+move $t4, $a0       # t4 holds address of array 
+
+loop:
+slt $t1, $t0, $a1	# check if x is less than arraylength
+beq $t1, $zero, end_loop
+
+sll $t2, $t0, 2 	# multiply x by 4 
+addu $t3, $t4, $t2	# add to address of array
+
+lw $a0, 0($t3)      # load word into $v0 
+li $v0, 1           # print what is in $a0
+syscall     
+
+# print a newline
+la $a0, newline
+li $v0, 4
+syscall
+
+addiu $t0, $t0, 1   # increment x 
+j loop 
+
+end_loop:
+jr $ra
